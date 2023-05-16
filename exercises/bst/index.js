e642b3707ebee3a39efe7a36403bba964b795445
1k4rkD50uz4 <47289876+1k4rkD50uz4@users.noreply.github.com>
2023-05-16 14:35:32 -0700

    bst soln:  create a node using an object and update the node by iterating over the node's key and updating the obj value

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -5,12 +5,47 @@
 // and 'right'.
 // 2) Implement the 'insert' method for the
 // Node class.  Insert should accept an argument
-// 'data', then create an insert a new node
+// 'data', then create and insert a new node
 // at the appropriate location in the tree.
 // 3) Implement the 'contains' method for the Node
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
-
+class Node {
+    constructor(data, left, right) {
+        this.data = { data: data };
+        this.left = { data: left };
+        this.right = { data: right };
+    }
+    insert(data) {
+        if (this.left.data < data) {
+            let temp = this.left.data;
+            if (this.left.right) {
+                if (this.left.right.data > data) {                    
+                    this.left.data = data;
+                    this.left.left = { data: temp };                    
+                }
+            }
+            else{
+                this.left.right = { data: data };
+            }
+        }   
+    }
+    contains(data) {
+        let res;
+        if (this.right.data > data) {
+            if (this.left.data < data) {
+                if (this.left.right.data == data) {
+                    res = this.left.right;
+                }
+            }            
+        }
+        return res;
+    }
+}
+//let people = ['Mark', 'Binaisha', 'Callen', 'Raynah', 'Hubert', 'Miriam', 'Kelwyn', 'Sandra'];
+//let bst = new Node('Mark', 'Binaisha', 'Miriam');
+//bst.insert('Callen');
+//bst.insert('Brielle');
+//bst.contains('Callen');
 module.exports = Node;
