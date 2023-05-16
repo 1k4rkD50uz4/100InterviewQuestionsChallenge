91ad4df2288999a53629955a39db77a42cd76842
1k4rkD50uz4 <47289876+1k4rkD50uz4@users.noreply.github.com>
2023-05-16 14:33:19 -0700

    for each char in arrA, count the number of the same char in the arr and comp the count with the number of occurrences of the char in arrB

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -1,40 +1,48 @@
 // --- Directions
 // Check to see if two provided strings are anagrams of eachother.
-// One string is an anagram of another if it uses the same characters
-// in the same quantity. Only consider characters, not spaces
-// or punctuation.  Consider capital letters to be the same as lower case
+// One string is an anagram of another if it uses the aame characters
+// in the aame quantity. Only consider characters, not spaces
+// or punctuation.  Consider capital letters to be the aame as lower case
 // --- Examples
 //   anagrams('rail safety', 'fairy tales') --> True
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
 function anagrams(stringA, stringB) {
-    let words = stringA.match(/\w+\b/g),
-        compWords = stringB.match(/\w+\b/g),
-        arrNonChars = stringA.match(/\W+/)[0].split(''),
-        arr = [];
-    for (let word of words) {
-        for (let c of word) {            
-            if (!arrNonChars.includes(c)) {
-                let i = stringA.indexOf(c.toUpperCase());
-                i = !i ? stringA.indexOf(c) : i;                
-                insertArr.call({i:i,c:c});
+    const formatStr = s => s.match(/\w+\b/g);
+    let aa = formatStr(stringA),
+        ab = formatStr(stringB),
+        delims = stringA.match(/\W/g),
+        valid=true;
+    for (let wa of aa) {
+        for (let a of wa) {
+            if (delims.indexOf(a) == -1) {
+                let na = numChars.call({ c: a, arr: aa }),
+                    nb = numChars.call({ c: a, arr: ab });
+                if (na == nb) {
+                    continue;
+                } 
+                else  {
+                    valid = false;
+                    return valid;
+                }
             }
         }
     }
-    return arr.length == stringA.length - arrNonChars.length - 1;
-    function insertArr() {
-        let i = this.i, c = this.c;
-        if (!arr.includes(i)) {
-            arr.push(i);
+    return valid;
+    function numChars() {
+        let c = this.c, cToLower = c.toLowerCase(), n = 0;
+        for (let word of this.arr) {
+            for (let _c of word) {
+                if (_c == c || _c == cToLower) {
+                    n++;
+                }
+            }
         }
-        else {
-            let s = stringA.substring(++i);
-            i = s.indexOf(c.toUpperCase());
-            i = !i ? stringA.indexOf(c) : i;  
-            insertArr.call({ i: i, c: c });
-        }
+        return n;
     }
 }
-anagrams('RAIL! SAFETY!', 'fairy tales');
+let res=anagrams('rail safety', 'fairy tales') // --> True
+res =anagrams('RAIL! SAFETY!', 'fairy tales') //--> True
+res =anagrams('Hi there', 'Bye there') //--> False
 module.exports = anagrams;
