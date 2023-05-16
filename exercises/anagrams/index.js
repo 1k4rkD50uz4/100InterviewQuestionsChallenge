2013e0d2712816f61db556febec0ca6f7bbed181
1k4rkD50uz4 <47289876+1k4rkD50uz4@users.noreply.github.com>
2023-05-16 16:58:06 -0700

    anagrams soln:  added check if both strings have the same number of chars

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -12,24 +12,27 @@ function anagrams(stringA, stringB) {
     const formatStr = s => s.match(/\w+\b/g);
     let aa = formatStr(stringA),
         ab = formatStr(stringB),
-        delims = stringA.match(/\W/g),
-        valid=true;
+        delims = stringA.match(/\W/g);
+    let as = aa.join(''),
+        bs = ab.join('');
+    if (as.length != bs.length) {
+        return false;
+    }
     for (let wa of aa) {
         for (let a of wa) {
-            if (delims.indexOf(a) == -1) {
+            if (delims && delims.indexOf(a) == -1) {
                 let na = numChars.call({ c: a, arr: aa }),
                     nb = numChars.call({ c: a, arr: ab });
                 if (na == nb) {
                     continue;
                 } 
                 else  {
-                    valid = false;
-                    return valid;
+                    return false;
                 }
             }
         }
     }
-    return valid;
+    return true;
     function numChars() {
         let c = this.c, cToLower = c.toLowerCase(), n = 0;
         for (let word of this.arr) {
@@ -42,7 +45,12 @@ function anagrams(stringA, stringB) {
         return n;
     }
 }
-let res=anagrams('rail safety', 'fairy tales') // --> True
-res =anagrams('RAIL! SAFETY!', 'fairy tales') //--> True
-res =anagrams('Hi there', 'Bye there') //--> False
+//let res=anagrams('rail safety', 'fairy tales') // --> True
+//res =anagrams('RAIL! SAFETY!', 'fairy tales') //--> True
+//res = anagrams('Hi there', 'Bye there') //--> False
+//res = anagrams('hello', 'llohe') //--> True
+//res = anagrams('Whoa! Hi!', 'Hi! Whoa!') //--> True
+//res = anagrams('One One', 'Two two two') //--> False
+//res = anagrams('One one', 'One one c') //--> False
+//res = anagrams('A tree, a life, a bench', 'A tree, a fence, a yard') //--> False
 module.exports = anagrams;
