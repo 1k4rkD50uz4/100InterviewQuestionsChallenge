cbc85cb6918e20f4789ab7c7a3a22805b301d281
1k4rkD50uz4 <mark.francis.dsouza@gmail.com>
2023-06-02 22:56:47 -0700

    capitalize soln using split by delim, map to capitalize the first letter and join by delim

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -8,30 +8,14 @@
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 //   capitalize('hi there, how is it going?') --> 'Hi There, How Is It Going?'
 
-// --- Solution:
-// Iterate over the characters in the string.If the character is a whitespace, save the index.  Use the index
-// to capitalize the next character and add the other characters in the word if they are not the index char.
-
 function capitalize(str) {
-    const inc = i => ++i;
-    let delim = ' ', s, j;
-    for (let [i, c] of Object.entries(str)) {
-        if (!s) {
-            s = c.toUpperCase();
-        }
-        else {
-            if (c == delim) {
-                j = +i;
-                c = str[inc(j)].toUpperCase();
-                s += delim + c;
-            }
-            else {
-                if (+i!=inc(j)) {
-                    s += c;
-                }
-            }
-        }       
-    }
-    return s;
+    let delim=' ';
+    return str
+        .split(delim)
+        .map(w=>{
+            let i=0;
+            return w[i++].toUpperCase()+w.substring(i);
+        })
+        .join(delim);
 }
 module.exports = capitalize;
